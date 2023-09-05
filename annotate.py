fe5dfafa5a985825ff41aea21c18fad0c489b0a4
David Steadman <wil.steadman@icloud.com>
2023-09-05 11:52:38 -0400

    POC box drawing

---
+++ annotate.py
@@ -0,0 +1,11 @@
+from PIL import Image, ImageDraw
+
+
+def annotate_boxes(screenshot_path, boxes):
+    screenshot = Image.open(screenshot_path)
+    draw = ImageDraw.Draw(screenshot)
+
+    for box in boxes:
+        draw.rectangle(box, width=10, outline="yellow")
+
+    screenshot.show()
